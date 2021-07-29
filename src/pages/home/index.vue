d35076c13c06d0a364d0e5d23c16d75842c1d923
xxljunjun <liaojunjun@autel.com>
2021-07-29 15:45:46 +0800

    完成首页UI布局

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -17,17 +17,49 @@
           </view>
         </swiper-item>
         <swiper-item>
-          <view class="swiper-item">B</view>
+          <view class="swiper-item">
+            <image src="/static/logo.png" class="swiper-img" />
+          </view>
         </swiper-item>
         <swiper-item>
-          <view class="swiper-item">C</view>
+          <view class="swiper-item">
+            <image src="/static/logo.png" class="swiper-img" />
+          </view>
         </swiper-item>
       </swiper>
     </view>
-    <!-- <view class="box"></view>
-    <view class="shop"></view>
+    <view class="box">
+      <view class="box-left">
+        <image src="/static/logo.png" class="bao" />
+        <view class="txt-1">自取</view>
+        <view class="txt-2">在线点，到店取</view>
+      </view>
+      <view class="box-right">
+        <image src="/static/logo.png" class="car" />
+        <view class="txt-1">外送</view>
+        <view class="txt-2">轻松点，送到家</view>
+      </view>
+    </view>
+    <view class="shop">
+      <view class="shop-left">
+        <image src="/static/logo.png" class="money" />
+        <view class="txt">
+          <view class="txt-1">积分商城</view>
+          <view class="txt-2">兑换潮流周边</view>
+        </view>
+      </view>
+      <view class="shop-right">
+        <image src="/static/logo.png" class="biao" />
+        <view class="txt">
+          <view class="txt-1">亦.会员</view>
+          <view class="txt-2">更多会员权益</view>
+        </view>
+      </view>
+    </view>
     <view class="title">周周有礼</view>
-    <view class="img-box"></view> -->
+    <view class="img-box">
+      <image src="/static/logo.png" class="banner" />
+    </view>
     <TabBar />
   </view>
 </template>
@@ -67,36 +99,109 @@ export default {
           width: 750rpx;
         }
       }
-      /deep/.uni-swiper-wrapper {
-        .uni-swiper-dots {
-          bottom: 200rpx;
-          .uni-swiper-dot {
-            width: 14rpx;
-            height: 14rpx;
-            margin-right: 16rpx;
-          }
-        }
-      }
     }
   }
   .box {
     width: 690rpx;
     height: 310rpx;
     border-radius: 20rpx;
-    box-shadow: 0 0 0 #000;
-    background: blue;
+    box-shadow: 0 10rpx 25rpx #ccc;
     margin: 0 auto;
     position: relative;
     top: -50rpx;
     margin-bottom: 30rpx;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 50rpx 0;
+    box-sizing: border-box;
+    background: #fff;
+    .box-left {
+      border-right: 1px solid #dbdbdb;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 345rpx;
+    }
+    .box-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 345rpx;
+    }
+    .bao {
+      height: 110rpx;
+      width: 110rpx;
+      margin-bottom: 20rpx;
+    }
+    .car {
+      height: 110rpx;
+      width: 110rpx;
+      margin-bottom: 16rpx;
+    }
+    .txt-1 {
+      font-size: 40rpx;
+      font-size: #000;
+      margin-bottom: 10rpx;
+      line-height: 46rpx;
+    }
+    .txt-2 {
+      font-size: 26rpx;
+      line-height: 32rpx;
+      color: #93c96d;
+    }
   }
   .shop {
     width: 690rpx;
     height: 170rpx;
     border-radius: 20rpx;
-    background: blue;
-    box-shadow: 0 0 0 #000;
+    background: #fff;
+    box-shadow: 0 10rpx 25rpx #ccc;
     margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 50rpx 0;
+    .txt-1 {
+      font-size: 36rpx;
+      line-height: 40rpx;
+      color: #000;
+      margin-bottom: 5rpx;
+    }
+    .txt-2 {
+      font-size: 26rpx;
+      line-height: 30rpx;
+      color: #969798;
+    }
+    .shop-left {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      width: 345rpx;
+      border-right: 1px solid #dbdbdb;
+      .money {
+        height: 65rpx;
+        width: 65rpx;
+        margin-right: 16rpx;
+      }
+    }
+    .shop-right {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      width: 345rpx;
+      .biao {
+        height: 65rpx;
+        width: 65rpx;
+        margin-right: 16rpx;
+      }
+    }
   }
   .title {
     margin-left: 30rpx;
@@ -108,8 +213,13 @@ export default {
     width: 690rpx;
     height: 290rpx;
     border-radius: 20rpx;
-    background: yellow;
     margin: 0 auto;
+    margin-bottom: 20rpx;
+    .banner {
+      width: 690rpx;
+      height: 290rpx;
+      border-radius: 20rpx;
+    }
   }
 }
 </style>

9fe1589951e001f478cecbd231f32c16cf676cde
xxljunjun <liaojunjun@autel.com>
2021-08-03 14:47:18 +0800

    点餐

---
+++ src/pages/eat/components/shop.vue
@@ -0,0 +1,105 @@
+<template>
+  <view class="shop">
+    <view class="shop-left">
+      <view
+        class="item"
+        :class="{ active: item.ischeck }"
+        v-for="(item, index) in listArr"
+        :key="index"
+        @click="clickItem(item)"
+        >{{ item.name }}</view
+      >
+    </view>
+    <view class="shop-right">22222222</view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      listArr: [
+        { id: 1, name: '当季限定', ischeck: true },
+        { id: 2, name: '奈雪必点', ischeck: false },
+        { id: 3, name: '中秋月饼', ischeck: false },
+        { id: 4, name: '霸气鲜果茶', ischeck: false },
+        { id: 5, name: '奈雪软欧包', ischeck: false },
+        { id: 6, name: '吐司/寿司', ischeck: false },
+        { id: 7, name: '奈雪零食', ischeck: false },
+        { id: 8, name: '无糖茶铺', ischeck: false },
+        { id: 9, name: '外卖推荐', ischeck: false },
+        { id: 11, name: '霸气鲜果茶', ischeck: false },
+        { id: 12, name: '奈雪软欧包', ischeck: false },
+        { id: 13, name: '吐司/寿司', ischeck: false },
+        { id: 14, name: '奈雪零食', ischeck: false },
+        { id: 15, name: '无糖茶铺', ischeck: false },
+        { id: 16, name: '外卖推荐', ischeck: false },
+        { id: 17, name: '外卖推荐', ischeck: false },
+        { id: 18, name: '霸气鲜果茶', ischeck: false },
+        { id: 19, name: '奈雪软欧包', ischeck: false },
+        { id: 20, name: '吐司/寿司', ischeck: false },
+        { id: 21, name: '奈雪零食', ischeck: false },
+        { id: 22, name: '无糖茶铺', ischeck: false },
+        { id: 23, name: '外卖推荐', ischeck: false },
+      ],
+    }
+  },
+  components: {},
+  methods: {
+    clickItem(val) {
+      console.log(val)
+      if (val.ischeck) {
+        return
+      }
+      this.listArr.forEach((ele) => {
+        if (ele.id == val.id) {
+          ele.ischeck = true
+        } else {
+          ele.ischeck = false
+        }
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.shop {
+  display: flex;
+  flex-direction: row;
+  .shop-left {
+    width: 170rpx;
+    background: #f5f5f5;
+    //左边滑动右边不滑动(通过固定定位和浮动实现)
+    position: fixed;
+    height: 100%;
+    overflow: auto;
+    float: left;
+    //不显示滚动条
+    &::-webkit-scrollbar {
+      width: 0;
+      height: 0;
+      color: transparent;
+    }
+    .item {
+      height: 90rpx;
+      width: 170rpx;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 22rpx;
+      color: #000;
+      &.active {
+        border-left: 10rpx solid #a7d500;
+        background: #fff;
+      }
+    }
+  }
+  .shop-right {
+    width: 580rpx;
+    background: #fff;
+    margin-left: 170rpx;
+  }
+}
+</style>
--- src/pages/eat/index.vue
+++ src/pages/eat/index.vue
@@ -1,21 +1,37 @@
 <template>
   <view class="eat">
-    <view class="status_bar"> <!-- 这里是状态栏 --> </view>
-    <view class="title"> 夏饮的茶 </view>
-    <view class="location">
-      <view class="location-left">
-        <view>西丽宝能城店></view>
-        <view>距离你669米</view>
+    <view class="fixd">
+      <view class="status_bar"> <!-- 这里是状态栏 --> </view>
+      <view class="title"> 夏饮的茶 </view>
+      <view class="location">
+        <view class="location-left">
+          <view class="txt-1">西丽宝能城店></view>
+          <view class="txt-2">距离你669米</view>
+        </view>
+        <view class="location-right">
+          <view class="txt-3">自取</view>
+          <view class="txt-4">外卖</view>
+        </view>
       </view>
-      <view class="location-right"> </view>
+      <view class="active">
+        <view class="active-left">
+          <image src="/static/logo.png" class="xing-icon" />
+          <view class="txt">夏日限定：储值300元，立赠一杯</view>
+        </view>
+        <view class="active-right">{{ '>' }}</view>
+      </view>
+      <view class="discounts">
+        <view class="discounts-left"> 外卖.牛轧糖买一送一 </view>
+        <view class="discounts-right">查看更多</view>
+      </view>
     </view>
-    <view class="active"> </view>
-    <view class="discounts"> </view>
+    <Shop />
     <TabBar />
   </view>
 </template>
 
 <script>
+import Shop from './components/shop.vue'
 import TabBar from '@/components/tabBar.vue'
 export default {
   data() {
@@ -28,6 +44,7 @@ export default {
   methods: {},
   components: {
     TabBar,
+    Shop,
   },
 }
 </script>
@@ -35,16 +52,134 @@ export default {
 <style lang="scss" scoped>
 .eat {
   color: #000;
-  //uniapp自带的变量控制状态栏
-  .status_bar {
-    height: var(--status-bar-height);
-    width: 100%;
-  }
-  .title {
-    margin: 0 auto;
-    text-align: center;
-    margin-top: 10rpx;
-    margin-bottom: 30rpx;
+  padding-top: 365rpx;
+  .fixd {
+    position: fixed;
+    top: 0;
+    background: #fff;
+    //uniapp自带的变量控制状态栏
+    .status_bar {
+      height: var(--status-bar-height);
+      width: 100%;
+    }
+    .title {
+      margin: 0 auto;
+      text-align: center;
+      margin-top: 10rpx;
+      margin-bottom: 30rpx;
+    }
+    .location {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 15rpx;
+      .location-left {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-left: 15rpx;
+        .txt-1 {
+          font-size: 28rpx;
+          color: #000;
+          margin-bottom: 20rpx;
+          line-height: 28rpx;
+        }
+        .txt-2 {
+          color: #a3a7a6;
+          font-size: 20rpx;
+          line-height: 20rpx;
+        }
+      }
+      .location-right {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+        margin-right: 15rpx;
+        color: #a0a5a8;
+        width: 175rpx;
+        height: 65rpx;
+        background: #f5f5f5;
+        border-radius: 45rpx;
+        .txt-3 {
+          width: 90rpx;
+          height: 55rpx;
+          background: #a7d500;
+          border-radius: 25rpx;
+          font-size: 22rpx;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+          color: #fff;
+        }
+        .txt-4 {
+          width: 90rpx;
+          height: 55rpx;
+          font-size: 22rpx;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+        }
+      }
+    }
+    .active {
+      height: 70rpx;
+      width: 750rpx;
+      background: #f9fafc;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      .active-left {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin-left: 20rpx;
+        .xing-icon {
+          height: 40rpx;
+          width: 40rpx;
+          margin-right: 10rpx;
+        }
+        .txt {
+          font-size: 22rpx;
+          color: #9dc020;
+        }
+      }
+      .active-right {
+        margin-right: 20rpx;
+      }
+    }
+    .discounts {
+      height: 80rpx;
+      width: 750rpx;
+      background: #ffffff;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      .discounts-left {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        margin-left: 20rpx;
+        width: 200rpx;
+        height: 40rpx;
+        border: 2rpx solid #9dc020;
+        border-radius: 10rpx;
+        color: #9dc020;
+        font-size: 18rpx;
+      }
+      .discounts-right {
+        margin-right: 20rpx;
+        color: #9dc020;
+        font-size: 18rpx;
+      }
+    }
   }
 }
 </style>
